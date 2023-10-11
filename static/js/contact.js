c6f0f45f9ea2c9174e551ad1e5baabdb97340e5e
MiKami12345 <sakaki299@gmail.com>
2023-10-12 01:15:44 +0900

    動的表示

--- static/js/contact.js
+++ static/js/contact.js
@@ -10,3 +10,30 @@ document.querySelector("#contact-form").addEventListener('submit', function(e) {
         document.querySelector("#contact-form").reset();
     });
 });
+
+const topicSelect = document.getElementById('topic');
+    const materialNameDiv = document.getElementById('material-name-div');
+    const accountNameDiv = document.getElementById('account-name-div');
+    const messageDiv = document.getElementById('message-div');
+    const submitDiv = document.getElementById('submit-div');
+
+    topicSelect.addEventListener('change', function () {
+        materialNameDiv.style.display = 'none';
+        accountNameDiv.style.display = 'none';
+        messageDiv.style.display = 'none';
+        submitDiv.style.display = 'none';
+
+        if (topicSelect.value === '教材について') {
+            materialNameDiv.style.display = 'block';
+            messageDiv.style.display = 'block';
+        } else if (topicSelect.value === 'アカウントについて') {
+            accountNameDiv.style.display = 'block';
+            messageDiv.style.display = 'block';
+        } else if (topicSelect.value === 'その他') {
+            messageDiv.style.display = 'block';
+        }
+
+        if(topicSelect.value) {
+            submitDiv.style.display = 'block';
+        }
+    });
